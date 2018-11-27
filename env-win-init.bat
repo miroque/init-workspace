2975d9ceceecefebb6da83ad0407905f0f6bc498
Alexandr Panov <i.miroque@gmail.com>
2018-11-27 12:09:38 +0700

    initial commit

---
+++ env-win-init.bat
@@ -0,0 +1,15 @@
+echo off
+cd c:
+cd \
+if not exist a.panov (
+echo Generatin initial personal folder
+mkdir a.panov
+echo go into it
+cd a.panov
+mkdir downloads
+mkdir tools
+mkdir work
+cd  work
+mkdir github
+mkdir gitlab
+)
