111924fa3834fa09278508ccbf2a4e3eecea5319
Alexandr Panov <i.miroque@gmail.com>
2018-11-27 16:02:21 +0000

    env-win-init.bat отредактирован онлайн на Bitbucket

--- env-win-init.bat
+++ env-win-init.bat
@@ -7,6 +7,7 @@ mkdir a.panov
 echo go into it
 cd a.panov
 mkdir downloads
+mkdir books
 mkdir tools
 mkdir work
 cd  work
@@ -14,6 +15,7 @@ mkdir github
 mkdir gitlab
 mkdir bitbucket
 cd ..
+echo *** generation complete ***
 ) else (
 echo Sorry dirrectory alredy exist
 )
