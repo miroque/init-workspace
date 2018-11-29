2ef4e5435804e8a23538dd0dbcbbd9460a9d9f6c
Alexandr Panov <i.miroque@gmail.com>
2018-11-29 14:09:49 +0700

    '#2' к этой задаче, только начало

--- env-win-init.bat
+++ env-win-init.bat
@@ -1,19 +1,21 @@
-echo off
-cd c:
-cd \
-if not exist a.panov (
-echo Generatin initial personal folder
-mkdir a.panov
-echo go into it
-cd a.panov
-mkdir downloads
-mkdir tools
-mkdir work
-cd  work
-mkdir github
-mkdir gitlab
-mkdir bitbucket
-cd ..
-) else (
-echo Sorry dirrectory alredy exist
-)
+powershell -Command Set-ExecutionPolicy RemoteSigned
+powershell -file env-win-init.ps1
+rem echo off
+rem cd c:
+rem cd \
+rem if not exist a.panov (
+rem echo Generatin initial personal folder
+rem mkdir a.panov
+rem echo go into it
+rem cd a.panov
+rem mkdir downloads
+rem mkdir tools
+rem mkdir work
+rem cd  work
+rem mkdir github
+rem mkdir gitlab
+rem mkdir bitbucket
+rem cd ..
+rem ) else (
+rem echo Sorry dirrectory alredy exist
+rem )
