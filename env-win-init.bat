b3d8adf5787d6c32ab02f92399b099ba7abdea79
Александр 'miroque' Панов <i.miroque@gmail.com>
2018-12-10 01:04:13 +0700

    базовая структура папок, с проверкой на существование

--- env-win-init.bat
+++ env-win-init.bat
@@ -1,5 +1,2 @@
-rem powershell "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File env-win-init.ps1' -Verb RunAs"
-rem powershell -Command Set-ExecutionPolicy RemoteSigned
-rem powershell -file env-win-init.ps1
 powershell -Command "Start-Process powershell 'Set-ExecutionPolicy RemoteSigned' -Verb RunAs"
 powershell -file env-win-init.ps1
--- env-win-init.ps1
+++ env-win-init.ps1
@@ -1,26 +1,49 @@
-Get-ExecutionPolicy
-echo "Going to Drive C:"
-echo "List the root catalog"
-
+Write-Output "Going to Drive C:"
 c:
 cd \
-Get-ChildItem
-<#
-rem echo off
-rem cd c:
-rem cd \
-rem if not exist a.panov (
-rem echo Generatin initial personal folder
-rem mkdir a.panov
-rem echo go into it
-rem cd a.panov
-rem mkdir downloads
-rem mkdir tools
-rem mkdir work
-rem cd  work
-rem mkdir github
-rem mkdir gitlab
-rem cd ..
-rem ) else (
-rem echo Sorry dirrectory alredy exist
-rem )#>
+
+$mainFolder = Read-Host 'What will be the main folder name?'
+
+# if exists
+If(!(Test-Path $mainFolder))
+{
+      New-Item -ItemType Directory -Force -Path $mainFolder
+} else
+{
+    Write-Output "[exists]$mainFolder"
+}
+
+Set-Location $mainFolder
+
+$firstLevelFolders = New-Object System.Collections.ArrayList
+[void]$firstLevelFolders.Add("downloads")
+[void]$firstLevelFolders.Add("tools")
+[void]$firstLevelFolders.Add("work")
+[void]$firstLevelFolders.Add("books")
+
+foreach ($item in $firstLevelFolders){
+    If(!(Test-Path $item))
+    {
+        New-Item -ItemType Directory -Force -Path $item
+    } else
+    {
+        Write-Output "[exists]`t$item"
+    }
+}
+
+Set-Location $firstLevelFolders[2]
+
+$secondLevelFolders = New-Object System.Collections.ArrayList
+[void]$secondLevelFolders.Add("github")
+[void]$secondLevelFolders.Add("gitlab")
+[void]$secondLevelFolders.Add("bitbucket")
+
+foreach ($item in $secondLevelFolders){
+    If(!(Test-Path $item))
+    {
+        New-Item -ItemType Directory -Force -Path $item
+    } else
+    {
+        Write-Output "[exists]`t$item"
+    }
+}
